3db1b36c1f5def222501dea85459c2a17ecb831c
camilooob <camilobaq@hotmail.com>
2019-11-29 21:47:14 -0500

    --- Auto-Git Commit ---

---
+++ .history/discounted_20191129213725.py
@@ -0,0 +1,26 @@
+# # Inputs
+price = 100
+# # Process
+discount30 = 0.3
+discount20 = 0.2
+discount10 = 0.1
+discount5 = 0.05
+discount0 = 0
+newprice
+
+if price >= 300:
+   newprice = price * discount30
+   print(newprice)
+elif price <= 200 and price < 300:
+    newprice = price * discount20
+    print(newprice)
+elif price <= 100 and price < 200:
+    newprice = price * discount10
+    print(newprice)
+elif price < 100 and price >= 0:
+   newprice = price * discount5
+   print(newprice)
+elif price < 0:
+    newprice = price
+    print(newprice)
+
---
+++ .history/discounted_20191129214651.py
@@ -0,0 +1,35 @@
+# # Inputs
+price = 100
+# # Process
+discount30 = 0.3
+discount20 = 0.2
+discount10 = 0.1
+discount5 = 0.05
+discount0 = 0
+descuento = 0
+newprice = 0
+
+if price >= 300:
+   descuento = price * discount30
+   print("discount= ", discount30)
+elif price >= 200 and price < 300:
+    descuento = price * discount20
+    newprice = price - descuento
+    print("discount= ", discount20)
+    print("price = ", newprice)
+elif price >= 100 and price < 200:
+    descuento = price * discount10
+    newprice = price - descuento
+    print("discount= ", discount30)
+	print("price = ", newprice)
+elif price < 100 and price >= 0:
+   descuento = price * discount5
+   newprice = price - descuento
+   print("discount= ", discount5)
+   print("price = ", newprice)
+elif price < 0:
+    descuento = price * discount0
+    newprice = price
+    print("No Discount")
+    print("price = ", newprice)
+
