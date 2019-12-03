62d3e0c9132b971106ecbd749d3b224a8b2c8d7a
camilooob <camilobaq@hotmail.com>
2019-12-03 12:05:41 -0500

    --- Auto-Git Commit ---

---
+++ .history/app/map_20191203114449.py
@@ -0,0 +1,5 @@
+num_list = [0, 1, 2, 3, 4, 5]
+
+double_list = map (lambda i : i * 2, num_list)
+
+print (list(double_list))
---
+++ .history/map_20191203114626.py
@@ -0,0 +1,5 @@
+num_list = [0, 1, 2, 3, 4, 5]
+
+double_list = map (lambda i : i * 2, num_list)
+# This creates a new list. The original list remains unchanged.
+print (list(double_list))
