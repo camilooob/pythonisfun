9aaa981b6ea1adcfcd311039bf47f078d09bc042
camilooob <camilobaq@hotmail.com>
2019-12-10 01:22:01 -0500

    my cubo

---
+++ cubo.py
@@ -0,0 +1,24 @@
+import turtle
+# genera ventana
+window = turtle.Screen()
+# nuevo dibujo 
+cuadrado = turtle.Turtle()
+# genera la linea con size 50
+cuadrado.forward(50)
+# voltemos la linea 90 grados
+cuadrado.left(90)
+# genera la linea con size 50
+cuadrado.forward(50)
+# voltemos la linea 90 grados
+cuadrado.left(90)
+# genera la linea con size 50
+cuadrado.forward(50)
+# voltemos la linea 90 grados
+cuadrado.left(90)
+# genera la linea con size 50
+cuadrado.forward(50)
+# voltemos la linea 90 grados
+cuadrado.left(90)
+
+turtle.mainloop()
+
