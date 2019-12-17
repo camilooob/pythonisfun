26c33210fa22b150a7ec99a007a71548c2d3bc0d
camilooob <camilobaq@hotmail.com>
2019-12-16 19:40:27 -0500

    =(

---
+++ .history/alphabet_20191216193554.py
@@ -0,0 +1,10 @@
+def alphabet_position(text):
+    num = 0
+    for i in range(1,len(text)):
+        num() = (ord(text(i) - 97)
+    return num
+
+def main():
+    print(alphabet_position("The sunset sets at twelve o' clock."))
+
+main()
